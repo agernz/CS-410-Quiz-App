c910541ff9762e9180b8a9f6704fac3bf58bcc7d
Adam J Gernes <agernz@gmail.com>
2018-05-11 13:42:14 -0500

    documentation

--- Quiz.py
+++ Quiz.py
@@ -1,9 +1,13 @@
+"""This class represents a quiz. It contains all the questions,
+the quiz id and length of quiz. A Quiz keeps track of the current
+question it is on and any dropped questions.
+"""
 class Quiz(object):
 
-    def __init__(self, questions, quiz_name, quiz_nr):
+    def __init__(self, questions, quiz_name, quiz_id):
         self.questions = questions
         self.name = quiz_name
-        self.nr = quiz_nr
+        self.id = quiz_id
         self.quiz_length = len(questions)
         self.index = 0
         self.score = 0
@@ -11,6 +15,9 @@ class Quiz(object):
 
 
     def display(self):
+        """Displays a question to the terminal by writing the question and
+        answer choices. May also drop questions that are not long enough
+        """
         # questions that are too short are likely not paresed correctly
         if len(self.questions[self.index][1]) > 10:
             print(self.questions[self.index][1])
@@ -23,22 +30,54 @@ class Quiz(object):
 
 
     def choice_is_correct(self, choice):
+        """Checks if the user input matches the question answer.
+        A correct answer increases the score
+
+        Keyword arguments:
+            choice -- user's input
+
+        Returns:
+            The correct answer if the user selected the wrong choice
+        """
         if (choice == self.questions[self.index][0].replace(" ", "").lower()):
             self.score += 1
             return None
         return self.questions[self.index][0]
 
     def mark_question(self, db):
-        db.mark_question(self.questions[self.index][1])
+        """Marks a question so that a user can study it later
+
+        Keyword arguments:
+            db -- a DBManager object
+        """
+        db.mark_question(self.questions[self.index][1], self.id)
 
     def question_is_marked(self, db):
-        return db.is_marked(self.questions[self.index][1])
+        """Checks if a question is marked in the database
+
+        Keyword arguments:
+            db -- a DBManager object
+
+        Returns:
+            True if the question is marked, False if not
+        """
+        return db.is_marked(self.questions[self.index][1], self.id)
 
     def next_question(self):
+        """Changes index to next question
+
+        Returns:
+            -1 if the index is invalid, this signifies that the quiz is over
+        """
         self.index += 1
         if self.index >= self.quiz_length:
             return -1
         return 1
 
     def get_score(self):
+        """
+        Returns:
+            The user's score on the quiz as a percentage with 100% being
+            the max score
+        """
         return (self.score / (self.quiz_length - self.num_dropped)) * 100
